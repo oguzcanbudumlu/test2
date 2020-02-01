7c866856e328e990f6223c3a3e57a7edfaff714f
OGUZCAN BUDUMLU <e2098820@ceng.metu.edu.tr>
2020-02-01 13:26:21 +0300

    Updates

---
+++ precache-manifest.59c76ad3cc14fc96dee8b83848bd22be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3668fb7d2f763e786c959ad6cae91aed",
+    "url": "/test2/index.html"
+  },
+  {
+    "revision": "8f4a422037d83e6295c5",
+    "url": "/test2/static/css/main.66490977.chunk.css"
+  },
+  {
+    "revision": "a48e0733997c6b7d0a0b",
+    "url": "/test2/static/js/2.1918f6f0.chunk.js"
+  },
+  {
+    "revision": "bc0c83fb5c6c8b946b94c2ec61dd7b90",
+    "url": "/test2/static/js/2.1918f6f0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8f4a422037d83e6295c5",
+    "url": "/test2/static/js/main.aafb3cc4.chunk.js"
+  },
+  {
+    "revision": "38c6f6eb7754a81163de",
+    "url": "/test2/static/js/runtime-main.2eecb3cc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test2/static/media/logo.5d5d9eef.svg"
+  }
+]);
